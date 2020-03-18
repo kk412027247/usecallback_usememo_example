04fd24f0799d80611638fd614862c9b5793a652e
TMD <kk412027247@gmail.com>
2020-03-18 20:39:54 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback, memo } from 'react';
 import './App.css';
 
+
+const randomColor = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+
+
+const Button = memo(props =>
+  <button onClick={props.onClick} style={{ background: randomColor() }}>
+    {props.children}
+  </button>
+);
+
+
+const functions = new Set();
+
 function App() {
+
+  const [delta, setDelta] = useState(1);
+  const [c, setC] = useState(0);
+
+  const incrementDelta = () => setDelta(delta => delta + 1);
+  const increment = () => setC(c => c + delta);
+
+
+  functions.add(incrementDelta);
+  functions.add(increment);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Delta is {delta}</div>
+      <div>Counter is {c} </div>
+      <br />
+      <div>
+        <Button onClick={incrementDelta}>Increment Delta</Button>
+        <Button onClick={increment}>increment Counter</Button>
+      </div>
+      <br />
+      <div>Newly Created Functions: {functions.size - 2}</div>
     </div>
   );
 }
+
 
 export default App;
