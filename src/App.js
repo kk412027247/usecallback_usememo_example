9fece72d00ab61954c67ae9c239a6dfaa053f0ab
TMD <kk412027247@gmail.com>
2020-03-18 22:11:10 +0800

    useMemo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { useState, useCallback, memo } from 'react';
+import React, {useState, useCallback, memo, useMemo} from 'react';
 import './App.css';
-
 
 const randomColor = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 
-
 const Button = memo(props =>
-  <button onClick={props.onClick} style={{ background: randomColor() }}>
+  <button onClick={props.onClick} style={{background: randomColor()}}>
     {props.children}
   </button>
 );
+
+
+// class Button extends React.Component {
+//
+//   shouldComponentUpdate(nextProps) {
+//     console.log(nextProps.onClick !== this.props.onClick, this.props.children !== nextProps.children);
+//     return nextProps.onClick !== this.props.onClick
+//   }
+//
+//   componentDidUpdate(prevProps, prevState, snapshot) {
+//     console.log('update')
+//   }
+//
+//   render() {
+//     return (
+//       <button onClick={this.props.onClick} style={{background: randomColor()}}>
+//         {this.props.children}
+//       </button>
+//     )
+//   }
+// }
 
 
 const functions = new Set();
@@ -18,28 +37,40 @@ function App() {
 
   const [delta, setDelta] = useState(1);
   const [c, setC] = useState(0);
+  const sinOfC = useMemo(() => Math.sin(c) + Math.random(), [c]);
 
-  const incrementDelta = () => setDelta(delta => delta + 1);
-  const increment = () => setC(c => c + delta);
+  const incrementDelta = useCallback(() => setDelta(delta => delta + 1), []);
+  const increment = useCallback(() => setC(c => c + delta), [delta]);
+  // const increment = useMemo(() => () => setC(c => c + delta), [delta]);
+
+  // const incrementBoth = useCallback(() => {
+  //   incrementDelta();
+  //   increment();
+  // }, [increment, incrementDelta]);
 
 
   functions.add(incrementDelta);
   functions.add(increment);
+  // functions.add(incrementBoth);
 
   return (
-    <div>
+    <>
       <div>Delta is {delta}</div>
       <div>Counter is {c} </div>
-      <br />
+      <div>Significant of C is {sinOfC}</div>
+      <br/>
       <div>
         <Button onClick={incrementDelta}>Increment Delta</Button>
         <Button onClick={increment}>increment Counter</Button>
       </div>
-      <br />
+      <br/>
       <div>Newly Created Functions: {functions.size - 2}</div>
-    </div>
+    </>
   );
 }
 
 
 export default App;
+
+
+// https://nikgrozev.com/2019/04/07/reacts-usecallback-and-usememo-hooks-by-example/
